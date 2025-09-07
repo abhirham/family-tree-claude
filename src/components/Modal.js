0b709e7f0adbaac1d1be852a402bf363f84075aa
abhirham <savarav.a@gmail.com>
2025-09-06 23:47:41 -0400

    code improvements

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -27,7 +27,7 @@ export default function Modal({ isOpen, onClose, children, title }) {
     <div className="fixed inset-0 z-50 flex items-center justify-center">
       {/* Backdrop */}
       <div 
-        className="absolute inset-0 bg-black bg-opacity-50 transition-opacity"
+        className="absolute inset-0 bg-black/30 transition-opacity"
         onClick={onClose}
       />
       
